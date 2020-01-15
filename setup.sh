b5d37ade609aec2d5e12d4638b2692b7c1a72b98
okayson <okayson1124@gmail.com>
2020-01-15 11:34:57 +0900

    Update setup.sh.

--- setup.sh
+++ setup.sh
@@ -8,8 +8,7 @@ TargetFiles=(.vimrc _gvimrc .vim)
 echo "make link ..."
 for target in ${TargetFiles[@]}
 do
-	echo "${LinkDir}${target} -> ${TargetDir}${target}"
-	ln -s ${TargetDir}${target} ${LinkDir}${target}
+	ln -s -v ${TargetDir}${target} ${LinkDir}${target}
 done
 
 exit
