7b509d1f7b2726577716e0eb5f0b57c22663bb3d
Kenichi Iwami <Kenichi.Iwami@omron.com>
2020-01-22 12:04:28 +0900

    Update setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-TargetDir=$(cd $(dirname $0); pwd)/
+LinkedDir=$(cd $(dirname $0); pwd)/
 LinkDir=~/
 
-TargetFiles=(.vimrc _gvimrc .vim)
+LinkedTargets=(.vimrc _gvimrc .vim)
 
 echo "[PROCESS] Make link ..."
 
-for target in ${TargetFiles[@]}
+for target in ${LinkedTargets[@]}
 do
 	if [[  -L ${LinkDir}${target}  ]]; then
 		echo "[INFO] Symbolic link exists...unlink: ${LinkDir}${target}"
@@ -18,21 +18,23 @@ do
 		echo "[ABORT] File exists: ${LinkDir}${target}"
 	else
 		echo "[INFO] Create symbolic link."
-		ln -s -v ${TargetDir}${target} ${LinkDir}${target}
+		ln -s -v ${LinkedDir}${target} ${LinkDir}${target}
 	fi
 done
 
 echo "[PROCESS] Update bashrc ..."
 
-AdditionalBashrc=${TargetDir}.bashrc
-grep "${AdditionalBashrc}" ~/.bashrc >/dev/null
-if [ $? -eq 0 ]; then
-	echo "[INFO] '${AdditionalBashrc}' is already setup in '~/.bashrc'."
-else
-	echo "[INFO] Setup '${AdditionalBashrc}' in '~/.bashrc'."
+BaseBashrc=~/.bashrc
+AdditionalBashrc=${LinkedDir}.bashrc
 
-	echo "" >> ~/.bashrc
-	echo "# Load additional .bashrc." >> ~/.bashrc
-	echo ". ${AdditionalBashrc}" >> ~/.bashrc
+grep "${AdditionalBashrc}" ${BaseBashrc} >/dev/null
+if [ $? -eq 0 ]; then
+	echo "[INFO] '${AdditionalBashrc}' is already setup in '${BaseBashrc}'."
+else
+	echo "[INFO] Setup '${AdditionalBashrc}' in '${BaseBashrc}'."
+	echo "" >> ${BaseBashrc}
+	echo "# Load additional .bashrc." >> ${BaseBashrc}
+	echo ". ${AdditionalBashrc}" >> ${BaseBashrc}
+	echo "[INFO] Please type '. ${AdditionalBashrc}' to reflect. "
 fi
 
