9960b54ad6664c3d6b813c0efee0a01d94d8647c
okayson <okayson1124@gmail.com>
2025-10-05 19:19:25 +0900

    Update `setup_windows.bat` for using nvim on windows.

--- setup_windows.bat
+++ setup_windows.bat
@@ -3,9 +3,7 @@
 set TARGET_DIR=
 cd %~dp0
 set TARGET_DIR=%cd%
-cd ../
-set LINK_DIR=%cd%
-cd %TARGET_DIR%
+set LINK_DIR=%UserProfile%
 
 set TARGET=.vimrc
 echo === %TARGET% ===
@@ -18,5 +16,15 @@ mklink %LINK_DIR%\%TARGET% %TARGET_DIR%\%TARGET%
 set TARGET=.vim
 echo === %TARGET% ===
 mklink /D %LINK_DIR%\%TARGET% %TARGET_DIR%\%TARGET%
+
+rem ------------------------------------
+rem XDG Base Directory Specification
+rem ------------------------------------
+set APP=nvim
+set TARGET=.config\%APP%
+echo === %TARGET% ===
+rem below directory is not used.
+rem mklink /D %LINK_DIR%\%TARGET% %TARGET_DIR%\%TARGET%
+mklink /D %LocalAppData%\%APP% %TARGET_DIR%\.config\%APP%
 
 pause
