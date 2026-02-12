cd8c54c6e454ca3e3f3d96b8093ce72b330fd795
okayson <okayson1124@gmail.com>
2026-02-13 08:17:12 +0900

    nvim: add japanese help.

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -12,6 +12,7 @@
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'vim-jp/vimdoc-ja', -- Japanese help for vim and neovim
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -63,8 +64,8 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- { import = 'custom.plugins' },
-    { import = 'plugins.copilot' },
-    { import = 'plugins.copilot-chat' },
+  { import = 'plugins.copilot' },
+  { import = 'plugins.copilot-chat' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
