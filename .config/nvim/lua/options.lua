32887fcfff85410a1c423fc57545c0404eec6fc4
okayson <okayson1124@gmail.com>
2026-02-13 08:13:37 +0900

    nvim: update whichwrap option.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -80,7 +80,8 @@ vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
 
-vim.opt.whichwrap:append('h,l,<,>,[,]')
+vim.opt.whichwrap:append('<,>,[,]')
+--vim.opt.whichwrap:append('h,l')
 
 -- set window title
 vim.o.title = true
