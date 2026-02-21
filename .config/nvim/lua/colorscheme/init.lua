6fb9a33edbc0566ed886e34226f96f7d9e93bfdc
okayson <okayson1124@gmail.com>
2026-02-21 18:23:08 +0900

    nvim: improve colorscheme mangement.

--- .config/nvim/lua/colorscheme/init.lua
+++ .config/nvim/lua/colorscheme/init.lua
@@ -1,10 +1,7 @@
--- Theme used at startup
-local DEFAULT_THEME = 'catppuccin'
---local DEFAULT_THEME = 'tokyonight'
---local DEFAULT_THEME = 'sonokai'
-local CURRENT = DEFAULT_THEME
+-- This module manages the colorscheme plugins and their configurations.
 
 -- List of themes
+-- The key is colorscheme plugin name. It is used as file name for the theme configuration in colorscheme/ directory.
 local themes = {
   catppuccin = {
     repo = 'catppuccin/nvim',
@@ -24,6 +21,51 @@ local themes = {
   },
 }
 
+local state_file = vim.fn.stdpath 'state' .. '/colorscheme.txt'
+
+local function save_colorscheme(colorscheme)
+  local f = io.open(state_file, 'w')
+  if f then
+    f:write(colorscheme)
+    f:close()
+  end
+end
+
+local function load_colorscheme()
+  local f = io.open(state_file, 'r')
+  if not f then
+    return nil
+  end
+  local colorscheme = f:read '*l'
+  f:close()
+  return colorscheme
+end
+
+local function guess_theme(colorscheme)
+  if not colorscheme then
+    return nil
+  end
+
+  for key, theme in pairs(themes) do
+    if colorscheme:match('^' .. theme.name) then
+      return key
+    end
+  end
+  return nil
+end
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    local colorscheme = vim.g.colors_name
+    save_colorscheme(colorscheme)
+  end,
+})
+
+-- Theme used at startup
+local DEFAULT_THEME = 'tokyonight'
+local saved_colorscheme = load_colorscheme()
+local current_theme = guess_theme(saved_colorscheme) or DEFAULT_THEME
+
 -- Generate plugin specifications for each theme
 local plugins = {}
 
@@ -33,17 +75,17 @@ for key, theme in pairs(themes) do
     name = theme.name,
 
     --Only the startup theme has lazy=false
-    lazy = key ~= CURRENT,
+    lazy = key ~= current_theme,
 
     -- Only the startup theme is given the highest priority.
-    priority = (key == CURRENT) and 1000 or nil,
+    priority = (key == current_theme) and 1000 or nil,
 
     config = function()
       -- Execute setup for each theme
       require('colorscheme.' .. key)()
 
       -- If it is the startup theme, apply it
-      if key == CURRENT then
+      if key == current_theme then
         vim.cmd.colorscheme(theme.name)
       end
     end,
