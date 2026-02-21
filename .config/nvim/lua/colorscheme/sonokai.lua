5519550e307cb2ed35c3cb5006e6795c3c7ce997
okayson <okayson1124@gmail.com>
2026-02-22 07:11:34 +0900

    nvim: udpate sonokai colorscheme.

--- .config/nvim/lua/colorscheme/sonokai.lua
+++ .config/nvim/lua/colorscheme/sonokai.lua
@@ -1,8 +1,12 @@
 return function()
   ---@diagnostic disable-next-line: missing-fields
-  --require('sonokai').setup {}
-  -- 'default',  'atlantis', 'andromeda', 'shusia', 'maia', 'espresso'
+  -- Set the style to use at the end of bellow.
+  vim.g.sonokai_style = 'espresso'
+  vim.g.sonokai_style = 'shusia'
+  vim.g.sonokai_style = 'default'
+  vim.g.sonokai_style = 'maia'
   vim.g.sonokai_style = 'atlantis'
+  vim.g.sonokai_style = 'andromeda'
 end
 
 -- vim: ts=2 sts=2 sw=2 et
